6ba4d403c1085928bb2467bb0a808aa5085b7963
szmulinho <samik1997@gmail.com>
2023-10-11 10:52:00 +0200

    update

---
+++ database/connection.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"github.com/szmulinho/common/config"
+	"github.com/szmulinho/common/model"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Connect() (*gorm.DB, error) {
+	connectionString := config.StorageConfig{}.ConnectionString()
+	conn := postgres.Open(connectionString)
+	db, err := gorm.Open(conn, &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(&model.Doctor{})
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
