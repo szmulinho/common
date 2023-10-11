febc7215829e561d0a43e5c790474e250c366af5
szmulinho <samik1997@gmail.com>
2023-10-11 14:36:21 +0200

    model update

--- internal/model/model.go
+++ internal/model/model.go
@@ -14,9 +14,9 @@ type Drug struct {
 }
 
 type CreatePrescInput struct {
-	PreID      int64  `json:"preid" gorm:"primaryKey;autoIncrement"`
+	PreID      int64  `json:"pre_id" gorm:"primaryKey;autoIncrement"`
 	Patient    string `json:"patient"`
-	Drugs      string `json:"drugs" gorm:"many2many:prescription_drugs;"`
+	Drugs      string `json:"drugs"`
 	Expiration string `json:"expiration"`
 }
 
