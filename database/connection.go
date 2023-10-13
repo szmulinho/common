af30704a82f7f3f12d18e2b9480efdf5e8467b28
szmulinho <samik1997@gmail.com>
2023-10-13 15:03:55 +0200

    config

--- database/connection.go
+++ database/connection.go
@@ -1,34 +1,14 @@
 package database
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
 	"github.com/szmulinho/common/config"
 	"github.com/szmulinho/common/model"
-	"os"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func LoadConfigFromEnv() config.StorageConfig {
-	return config.StorageConfig{
-		Host:     os.Getenv("DB_HOST"),
-		User:     os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Dbname:   os.Getenv("DB_NAME"),
-		Port:     os.Getenv("DB_PORT"),
-	}
-}
-
-func init() {
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("Error loading .env file")
-	}
-}
-
 func Connect() (*gorm.DB, error) {
-	conn := LoadConfigFromEnv()
+	conn := config.LoadConfigFromEnv()
 	connectionString := conn.ConnectionString()
 
 	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
