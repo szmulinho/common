dd36c55eb12bfa341490d4a24872ee2db47c0e52
szmulinho <samik1997@gmail.com>
2023-11-06 19:11:37 +0100

    model

--- model/model.go
+++ model/model.go
@@ -24,6 +24,7 @@ type Prescription struct {
 
 type Opinion struct {
 	ID      int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Login   string `json:"login"`
 	Rating  int    `json:"rating" gorm:"column:rating"`
 	Comment string `json:"comment"`
 }
